532432498a7ee2c9046b7a100aec2e4176cb976c
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2024-06-22 12:30:26 +0300

    Start of working on exercise 2

---
+++ pe242_code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define DumVal 0
+
+const int M = 2;       // αριθμός σταθμών (i)
+const int C = 3;       // αριθμός κατηγοριών (j)
+const int N = 256.776; // αριθμός εργασιών (k)
+int n[C + 1];
+
+/* 1. Dcpu */
+/* 2. Ddisk */
+/* 3. udisk */
+/* 4. (κάποιος χρόνος) */
